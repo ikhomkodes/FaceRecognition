3a50c21b3ff9311dd075a9b54e1cc6134ee23c19
ikhom <koderbouza37@gmail.com>
2021-04-17 15:10:56 +0545

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from cx_Freeze import setup, Executable
-import sys,os
+import sys, os
+
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.>
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 base = None
@@ -9,25 +10,24 @@ base = None
 if sys.platform == 'win32':
     base = None
 
-
 executables = [Executable("train.py", base=base)]
 
-packages = ["idna","os","sys","cx_Freeze","tkinter","cv2","setup",
-            "numpy","PIL","pandas","datetime","time"]
+packages = ["idna", "os", "sys", "cx_Freeze", "tkinter", "cv2", "setup",
+            "numpy", "PIL", "pandas", "datetime", "time"]
 options = {
     'build_exe': {
 
-        'packages':packages,
+        'packages': packages,
     },
 
 }
 
 setup(
-    name = "ToolBox",
-    options = options,
-    version = "0.0.1",
-    description = 'Vision ToolBox',
-    executables = executables
+    name="ToolBox",
+    options=options,
+    version="0.0.1",
+    description='Vision ToolBox',
+    executables=executables
 )
 
-#write python setup build
+# write python setup build
